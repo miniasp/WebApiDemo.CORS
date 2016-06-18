19dce9bdbef26c485817132d46d40a6837a4a4dc
Will 保哥 <doggy.huang@gmail.com>
2016-06-18 22:22:46 +0800

    Initial commit

---
+++ WebService/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebService.Controllers
+{
+    public class TestController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("GET: Test message")
+            };
+        }
+
+        public HttpResponseMessage Post()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("POST: Test message")
+            };
+        }
+
+        public HttpResponseMessage Put()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("PUT: Test message")
+            };
+        }
+    }
+}
